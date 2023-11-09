91f19712887199af7a82c859256f0d788bdf4b6b
haodedong <haodedong1996@163.com>
2023-11-09 20:58:42 +0800

    init

---
+++ src/main/java/com/hdd/winterSolsticeBolg/WinterSolsticeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hdd.winterSolsticeBolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSolsticeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterSolsticeBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdd/winterSolsticeBolg/WinterSolsticeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdd.winterSolsticeBolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSolsticeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
